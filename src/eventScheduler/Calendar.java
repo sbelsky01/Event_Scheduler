f5342321fc3934f7a4ff8c0dbdd85f57d21a50ec
ShiraNeumann <102998234+ShiraNeumann@users.noreply.github.com>
2023-01-01 23:51:42 -0500

    Added modify methods

--- src/eventScheduler/Calendar.java
+++ src/eventScheduler/Calendar.java
@@ -21,13 +21,13 @@ public class Calendar {
 			pstmt.setInt(1, userId);
 			pstmt.setDate(2, Date.valueOf(appt.getDate()));
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				if(appt.during(rs.getDate(2).toLocalDate(), rs.getTime(3).toLocalTime(), rs.getInt(4))) {
+			while (rs.next()) {
+				if (appt.during(rs.getDate(2).toLocalDate(), rs.getTime(3).toLocalTime(), rs.getInt(4))) {
 					return rs.getInt(1);
 				}
 			}
 
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 
@@ -40,14 +40,14 @@ public class Calendar {
 			int newId;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT max(e.eventid) FROM event e");
-			newId = rs.next() ? rs.getInt(1)+1 : 1;
+			newId = rs.next() ? rs.getInt(1) + 1 : 1;
 
-			//write the insert statement
+			// write the insert statement
 			String SQL = "INSERT INTO event (EventId, EventName, StreetNum, StreetName, "
 					+ "City, State, Zip, Date, Time, Duration, Notes, CategoryId) "
 					+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?);"
 					+ "INSERT INTO personhasevent (PersonId, EventId) VALUES (?, ?)";
-			//prepare the statement
+			// prepare the statement
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			Address addr = appt.getAddress();
 			pstmt.setInt(1, newId);
@@ -64,74 +64,72 @@ public class Calendar {
 			pstmt.setString(12, appt.getCategory());
 			pstmt.setInt(13, userId);
 			pstmt.setInt(14, newId);
-			//execute the statement
+			// execute the statement
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	public void displayEvents(int colName) {
+	public void displayEvents(String colName) {
 		try {
-			String SQL = "SELECT * "
-					+ "FROM event e JOIN personhasevent phe ON e.eventid = phe.eventid "
-					+ "JOIN categorytype c ON e.categoryid = c.categoryid "
-					+ "WHERE phe.personId = ?"
-					+ "ORDER BY ?";
-			//prepare the statement
-			PreparedStatement pstmt;
-			pstmt = conn.prepareStatement(SQL);
+			String SQL = "SELECT * FROM event e " + "JOIN personhasevent phe ON e.eventid = phe.eventid "
+					+ "JOIN categorytypes c ON e.categoryid = c.categoryid " + "WHERE phe.personId = ? " + "ORDER BY e."
+					+ colName + " ASC";
+			// prepare the statement
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, userId);
-			pstmt.setString(2, colName);
 			ResultSet rs = pstmt.executeQuery();
-			
-			//display the results, and display a message if there are no results
-			if(!rs.next()) {
+
+			// display the results, and display a message if there are no results
+			if (!rs.next()) {
 				System.out.println("You have no events scheduled.");
 			} else {
-				Appointment appt;
-				Address addr;
 				do {
-					addr = new Address(rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-					appt = new Appointment(rs.getDate(9).toLocalDate(), rs.getString(2), rs.getTime(8).toLocalTime(), rs.getString(11), rs.getInt(10), addr, rs.getString(13));
-					System.out.println();
 					System.out.println("#" + rs.getInt(1));
-					System.out.println(appt);
+					System.out.println("\t" + rs.getString(2));
+					System.out.print("\tDate: ");
+					System.out.println(
+							rs.getDate(9).toLocalDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+					System.out.print("\tTime: ");
+					System.out.println(rs.getTime(8).toLocalTime().format(DateTimeFormatter.ofPattern("hh:mm a")));
+					System.out.print("\t" + rs.getInt(3) + " " + rs.getString(4) + " ");
+					System.out.println(rs.getString(5) + ", " + rs.getString(6) + "  " + rs.getString(7));
+					System.out.println("\tCategory: " + rs.getString(16));
+					System.out.println("\t" + rs.getString(11));
 					System.out.println();
-					
-				}while(rs.next());
+				} while (rs.next());
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void displayShortEvents() {
 		try {
 			String SQL = "SELECT e.EventId, EventName "
-					+ "FROM event e JOIN personhasevent phe ON e.eventid = phe.eventid "
-					+ "WHERE phe.personId = ?";
-			//prepare the statement
+					+ "FROM event e JOIN personhasevent phe ON e.eventid = phe.eventid " + "WHERE phe.personId = ?";
+			// prepare the statement
 			PreparedStatement pstmt;
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, userId);
 			ResultSet rs = pstmt.executeQuery();
-			
-			//display the results, and display a message if there are no results
-			if(!rs.next()) {
+
+			// display the results, and display a message if there are no results
+			if (!rs.next()) {
 				System.out.println("You have no events scheduled.");
 			} else {
 				do {
 					System.out.println();
 					System.out.print("#" + rs.getInt(1) + " ");
 					System.out.println(rs.getString(2));
-					
-				}while(rs.next());
+
+				} while (rs.next());
 			}
 			System.out.println();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -140,19 +138,32 @@ public class Calendar {
 
 	public boolean displayEvent(int id) {
 		try {
-			String SQL = "SELECT * "
-					+ "FROM event e JOIN personhasevent phe ON e.eventId = phe.eventId "
-					+ "JOIN categorytype c ON e.categoryid = c.categoryid "
+			String SQL = "SELECT * " + "FROM event e JOIN personhasevent phe ON e.eventId = phe.eventId "
+					+ "LEFT JOIN categorytypes c ON e.categoryid = c.categoryid "
 					+ "WHERE phe.personId = ? AND e.eventId = ?";
-			//prepare the statement
+			// prepare the statement
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, userId);
 			pstmt.setInt(2, id);
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
-				Address addr = new Address(rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-				Appointment appt = new Appointment(rs.getDate(9).toLocalDate(), rs.getString(2), rs.getTime(8).toLocalTime(), rs.getString(11), rs.getInt(10), addr, rs.getString(13));
-				System.out.println("\n" + appt + "\n");
+			if (rs.next()) {
+//				Address addr = new Address(rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6),
+//						rs.getString(7));
+//				Appointment appt = new Appointment(rs.getDate(9).toLocalDate(), rs.getString(2),
+//						rs.getTime(8).toLocalTime(), rs.getString(11), rs.getInt(10), addr, rs.getString(13));
+//				System.out.println("");
+//				System.out.println("\n" + appt + "\n");
+				System.out.print("Date: ");
+				System.out.println(
+						rs.getDate(9).toLocalDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+				System.out.println("Time: ");
+				System.out.println(rs.getTime(8).toLocalTime().format(DateTimeFormatter.ofPattern("hh:mm a")));
+				System.out.println("Category: " + rs.getString(14));
+				System.out.println(rs.getString(2));
+				System.out.print(rs.getInt(3) + " " + rs.getString(4) + " ");
+				System.out.println(rs.getString(5) + ", " + rs.getString(6) + "  " + rs.getString(7));
+				System.out.println(rs.getString(11));
+				System.out.println();
 				return true;
 			}
 		} catch (SQLException e) {
@@ -164,8 +175,7 @@ public class Calendar {
 
 	public void deleteEvent(int id) {
 		try {
-			String SQL = "DELETE FROM personhasevent WHERE EventId = ?;"
-						+ "DELETE FROM event WHERE EventId = ?;";
+			String SQL = "DELETE FROM personhasevent WHERE EventId = ?;" + "DELETE FROM event WHERE EventId = ?;";
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, id);
 			pstmt.setInt(2, id);
@@ -174,14 +184,14 @@ public class Calendar {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	public void setId(int id) {
 		this.userId = id;
 	}
-	
+
 	public void sendEmail() {
 		Email email = new Email(getUserEmail(), "Event Created", getNewestEvent());
-		
+
 	}
 
 	public String getUserEmail() {
@@ -205,10 +215,9 @@ public class Calendar {
 	}
 
 	public String getNewestEvent() {
-		//Sort in descending order and only allow one to get the last event added
+		// Sort in descending order and only allow one to get the last event added
 		String SQL = "SELECT * " + "FROM event e JOIN personhasevent phe ON e.eventId = phe.eventId "
-				+ "WHERE phe.personId = ? " +
-				"ORDER BY e.eventid DESC LIMIT 1";
+				+ "WHERE phe.personId = ? " + "ORDER BY e.eventid DESC LIMIT 1";
 		// prepare the statement
 		PreparedStatement pstmt;
 		String event = null;
@@ -217,14 +226,13 @@ public class Calendar {
 
 			pstmt.setInt(1, userId);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			if (rs.next()) {
-				
 
 				Address addr = new Address(rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6),
 						rs.getString(7));
 				Appointment appt = new Appointment(rs.getDate(9).toLocalDate(), rs.getString(2),
-						rs.getTime(8).toLocalTime(), rs.getString(11), rs.getInt(10), addr);
+						rs.getTime(8).toLocalTime(), rs.getString(11), rs.getInt(10), addr, rs.getString(12));
 
 				event = "\n" + appt + "\n";
 			}
@@ -233,6 +241,115 @@ public class Calendar {
 			System.out.println(e.getMessage());
 		}
 		return event;
+	}
+
+	public void modifyName(int id, String name) {
+
+		String SQL = "UPDATE event SET eventname = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, name);
+			pstmt.setInt(2, id);
+			ResultSet rs = pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void modifyDate(int id, LocalDate date) {
+		String SQL = "UPDATE event e SET date = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setDate(1, Date.valueOf(date));
+			pstmt.setInt(2, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void modifyTime(int id, LocalTime time) {
+		String SQL = "UPDATE event e SET time = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setTime(1, Time.valueOf(time));
+			pstmt.setInt(2, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void modifyAddress(int id, Address address) {
+		String SQL = "UPDATE event e SET streetnum = ?, streetname = ?, city = ?, state = ?, zip = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, address.getNumber());
+			pstmt.setString(2, address.getStreet());
+			pstmt.setString(3, address.getCity());
+			pstmt.setString(4, address.getState());
+			pstmt.setString(5, address.getZIP());
+			pstmt.setInt(6, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void modifyDuration(int id, int duration) {
+		String SQL = "UPDATE event e SET duration = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, duration);
+			pstmt.setInt(2, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void modifyDuration(int id, String notes) {
+		String SQL = "UPDATE event e SET notes = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, notes);
+			pstmt.setInt(2, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public void modifyCategory(int id, String category) {
+		String SQL = "UPDATE categorytypes SET categorydescription = ? WHERE eventid = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, category);
+			pstmt.setInt(2, id);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
